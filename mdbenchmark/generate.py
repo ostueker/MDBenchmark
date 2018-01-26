746053088a742ce4eab0d9492b4cf80d28634aac
Marc Siggel <marcsiggel@Marcs-MacBook-Air.local>
2018-01-26 09:36:57 +0100

    refactored code to allow for additional md engines

--- mdbenchmark/generate.py
+++ mdbenchmark/generate.py
@@ -26,58 +26,29 @@ import mdsynthesis as mds
 from jinja2.exceptions import TemplateNotFound
 
 from .cli import cli
-from .utils import ENV, normalize_host, print_possible_hosts
+from .utils import ENV, normalize_host, print_possible_hosts, detect_md_engine
+import mdengines.gromacs
+import mdengines.namd
 
-
-def write_bench(top, tmpl, nodes, gpu, module, name, host, time):
-    sim = mds.Sim(
-        top['{}/'.format(nodes)],
-        categories={
-            'module': module,
-            'gpu': gpu,
-            'nodes': nodes,
-            'host': host,
-            'time': time,
-            'name': name,
-            'started': False
-        })
-
-    # copy input files
-    tpr = '{}.tpr'.format(name)
-
-    if not os.path.exists(tpr):
-        raise click.FileError(
-            tpr, hint='File does not exist or is not readable.')
-
-    copyfile(tpr, sim[tpr].relpath)
-
-    # Add some time buffer to the requested time. Otherwise the queuing system
-    # kills the jobs before GROMACS can finish
-    formatted_time = '{:02d}:{:02d}:00'.format(*divmod(time + 5, 60))
-
-    # create bench job script
-    script = tmpl.render(
-        name=name,
-        gpu=gpu,
-        module=module,
-        n_nodes=nodes,
-        time=time,
-        formatted_time=formatted_time)
-
-    with open(sim['bench.job'].relpath, 'w') as fh:
-        fh.write(script)
-
+#import mdengines
 
 @cli.command()
 @click.option(
     '-n',
     '--name',
-    help='name of tpr file',
-    default='md',
+    help='name of the input file',
+    default='',
     show_default=True)
 @click.option(
-    '-g', '--gpu', is_flag=True, help='run on gpu as well', show_default=True)
-@click.option('-m', '--module', help='gromacs module to use', multiple=True)
+    '-g',
+    '--gpu',
+    is_flag=True,
+    help='run on gpu as well',
+    show_default=True)
+@click.option('-m',
+    '--module',
+    help='module of the MD engine to use',
+    multiple=True)
 @click.option('--host', help='job template name', default=None)
 @click.option(
     '--max-nodes',
@@ -99,7 +70,7 @@ def write_bench(top, tmpl, nodes, gpu, module, name, host, time):
     type=click.IntRange(1, 1440))
 @click.option('--list-hosts', help='show known hosts', is_flag=True)
 def generate(name, gpu, module, host, max_nodes, min_nodes, time, list_hosts):
-    """Generate benchmark queuing jobs.
+    """Generate benchmark queuing jobs
     """
     if list_hosts:
         print_possible_hosts()
@@ -117,6 +88,8 @@ def generate(name, gpu, module, host, max_nodes, min_nodes, time, list_hosts):
         raise click.BadParameter(
             'You did not specify which gromacs module to use for scaling.',
             param_hint='"-m" / "--module"')
+    #detect the engine and use accordingly
+
 
     # Provide some output for the user
     number_of_mdbenchmarks = click.style(
@@ -128,6 +101,9 @@ def generate(name, gpu, module, host, max_nodes, min_nodes, time, list_hosts):
     for m in module:
         directory = '{}_{}'.format(host, m)
         gpu_string = '.'
+        #here we switch between the md engines like gromacs and namd
+        #see the mdengines modules
+        engine = detect_md_engine(m)
 
         if gpu:
             directory += '_gpu'
@@ -140,7 +116,7 @@ def generate(name, gpu, module, host, max_nodes, min_nodes, time, list_hosts):
             gromacs_module, gpu_string))
 
         for n in range(min_nodes, max_nodes + 1):
-            write_bench(top, tmpl, n, gpu, m, name, host, time)
+             engine.write_bench(top, tmpl, n, gpu, m, name, host, time)
 
     click.echo('Finished generating all benchmark systems.')
     click.echo('Now run `mdbenchmark start` to submit jobs.')
