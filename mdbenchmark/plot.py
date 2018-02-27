88cbbebffc4f93fe61375fc0e3b4292d1b94bc27
Max Linke <max_linke@gmx.de>
2018-02-27 19:45:48 +0100

    fix plot

--- mdbenchmark/plot.py
+++ mdbenchmark/plot.py
@@ -24,7 +24,7 @@ from matplotlib.figure import Figure
 import click
 
 from .cli import cli
-from .util import calc_slope_intercept, lin_func
+from .utils import calc_slope_intercept, lin_func
 
 
 def plot_line(df, label, ax=None):
@@ -33,15 +33,25 @@ def plot_line(df, label, ax=None):
 
     p = ax.plot('nodes', 'ns/day', '.-', data=df, ms='10', label=label)
     color = p[0].get_color()
-    slope, intercept = calc_slope_intercept((df['nodes'].iloc[0], df['ns/day'].iloc[0]),
-                                            (df['nodes'].iloc[1], df['ns/day'].iloc[1]))
-    ax.plot(df['nodes'], lin_func(df['nodes'], slope, intercept), ls='--', color=color, alpha=.5)
+    slope, intercept = calc_slope_intercept(
+        (df['nodes'].iloc[0], df['ns/day'].iloc[0]), (df['nodes'].iloc[1],
+                                                      df['ns/day'].iloc[1]))
+    # avoid a label and use values instead of pd.Series
+    ax.plot(
+        df['nodes'],
+        lin_func(df['nodes'].values, slope, intercept),
+        ls='--',
+        color=color,
+        alpha=.5)
     return ax
 
 
 def plot_over_group(df, groupby, ax=None):
     # plot all lines
-    gb = df.groupby(groupby.split(','))
+    print(df.head())
+    print(groupby)
+    print(df.module)
+    gb = df.groupby(groupby)
     for key, df in gb:
         plot_line(ax=ax, df=df, label=key)
 
@@ -54,15 +64,14 @@ def plot_over_group(df, groupby, ax=None):
     ax1_xticks = ax.get_xticks()
     ax2.set_xticks(ax1_xticks)
     ax2.set_xbound(ax.get_xbound())
-    ax2.set_xticklabels(gb.get_group(gb.groups[0])['ncores'])
+    ax2.set_xticklabels(gb.get_group(list(gb.groups.keys())[0])['ncores'])
     ax2.set_xlabel('{}'.format('{}\n\nCores'.format(df['host'][0])))
 
     return ax
 
 
 @cli.command()
-@click.option(
-    'csv', help='name of csv file')
+@click.option('--csv', help='name of csv file')
 @click.option(
     '--groupby',
     '-g',
@@ -71,7 +80,8 @@ def plot_over_group(df, groupby, ax=None):
     default='gpu, module',
     show_default=True)
 def plot(csv, groupby):
-    df = pd.read_csv(csv)
+    """Plot nice things"""
+    df = pd.read_csv(csv, index_col=0)
     # Remove NaN values. These are missing ncores/performance data.
     df = df.dropna()
     # We have to use the matplotlib object-oriented interface directly, because
@@ -80,5 +90,7 @@ def plot(csv, groupby):
     fig = Figure()
     FigureCanvas(fig)
     ax = fig.add_subplot(111)
-    plot_over_group(df, groupby.split(','), ax=ax)
+    # remove whitespace
+    groupby = [s.strip() for s in groupby.split(',')]
+    plot_over_group(df, groupby, ax=ax)
     fig.savefig('results.pdf')
